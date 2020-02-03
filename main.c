ce41ab81bda90e720b359b05d4792a950aa50666
thomas <swanzem11@gmail.com>
2020-02-02 21:00:39 -0800

    Get user args, print envpath

---
+++ main.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+char *args[64];
+
+int main(int argc, char** argv, char **envp)
+{
+  print_path(); // print path
+  while(1)
+  {
+    get_args(); // get args
+    memset(args, '\0', sizeof(args)); // clear args
+  }
+}
+
+void print_path() {
+  char* s = getenv("PATH");
+  printf("PATH: \n%s\n", (s != 0) ? s : "getenv returned NULL");
+
+  char *token = strtok(s, ":");
+
+  printf("PATH STRINGS:\n");
+  while(token != NULL) {
+    printf("%s ", token);
+    token = strtok(NULL, ":");
+  }
+
+  printf("\n");
+}
+
+void get_args() { // retrieves args from input and saves to global array
+    char temp[128], line[128];
+    int i = 0;
+
+    printf("\ncs360 $ ");
+		fgets(temp, sizeof(temp), stdin);
+		line[strlen(line)-1] = 0;
+		
+    strcpy(line, "/bin/");
+    strcat(line, temp);
+
+    char *token = strtok(line, " ");
+
+    while(token != NULL) {
+      char *str = (char*)calloc(strlen(token), sizeof(char));
+      strcpy(str, token);
+      args[i] = str;
+      token = strtok(NULL, " ");
+      i++;
+    }
+
+
+    args[i-1][strlen(args[i-1])-1] = 0; // remove extra char from last arg
+
+    args[i] = NULL; // terminating null pointer for execv
+}
---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <stddef.h> // NULL
+
+void print_path();
+void get_args();
+
+#endif
