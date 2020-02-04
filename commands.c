d2e458107574f90ea1c3f2a6d9de07f3159731a2
John <john@host-J.local>
2020-02-03 22:20:39 -0800

    Added executible command handler (Will fork process and call using execve() if executible exists in any evironmental variable PATH directory)

--- commands.c
+++ commands.c
@@ -1,6 +1,9 @@
 #include "commands.h"
 
 extern char *args[64];
+extern char line[128];
+extern char *paths[64];
+extern char **env;
 
 char *cmds[ ] = {"exit", 0};
 int(*fptr[ ])()={ command_exit};
@@ -23,7 +26,56 @@ void command_parse()
             break;
         }
     }
-
+    //If command wasn't found in the defined command array
     if(i != -1)
-        printf("invalid command\n");
+    {
+        char filepath[512];
+        int j = 0;
+        while(paths[j])
+        {
+            strcpy(filepath, paths[j++]);
+            strcat(filepath, "/");
+            strcat(filepath, args[0]);
+            //printf("FILEPATH: %s\n", filepath);                       //**DEBUG** : prints all tested filepaths
+            if(access(filepath, F_OK) != -1)
+            {
+                //printf("FILEPATH LOCATED:: %s\n\n", filepath);        //**DEBUG** : prints valid filepath if found
+                j = -1;
+                break;
+            }
+        }
+
+        //If executible file was foundall tested filepaths
+        if(j == -1)
+        {
+            int pid;
+            pid = fork();
+            if(pid)
+            {
+                //parent executes this part
+                int dead_pid, exitval;
+                //wait for child process to terminate
+                dead_pid = wait(&exitval);
+                printf("Process %d exited with code %d\n", dead_pid, exitval);
+            }
+            else
+            {
+                //child Executes this part;
+                execve(filepath, args, env);
+                //execve should never return since it replaces the calling process
+                //if it returns, tell terminal an error occured and exit process
+                printf("**ERROR**");
+                exit(-1);
+            }
+        }
+
+        //Command not found in command array or as executible in all environmental variable paths
+        else
+        {
+            printf("Invalid Command\n");
+        }
+        
+
+    }
+    //TODO  recognize invalid command and print to terminal
 }
--- commands.h
+++ commands.h
@@ -5,6 +5,8 @@
     #include <stdio.h>
     #include <stdlib.h>
     #include <string.h>
+    #include <unistd.h>
+    #include <wait.h>
 
     /* Declarations */
     void command_parse();
